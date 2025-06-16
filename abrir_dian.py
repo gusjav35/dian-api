3b93a74350e8b79d675f6830383bc7859ba57124
Gustavo <gustavojavier.franco88@gmail.com>
2025-06-16 12:47:37 -0500

    Update abrir_dian.py

--- abrir_dian.py
+++ abrir_dian.py
@@ -34,7 +34,7 @@ def intentar_consulta(nit: str):
         boton = driver.find_element(By.ID, "vistaConsultaEstadoRUT:formConsultaEstadoRUT:btnBuscar")
         location = boton.location
 
-        x = location["x"] + 55
+        x = location["x"] + 62
         y = location["y"] + 60
 
         pyautogui.moveTo(x, y, duration=1)
